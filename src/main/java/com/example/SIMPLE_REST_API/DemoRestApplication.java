561d28b7a34bdc069f30e27ba94b960e43c894a3
924394293492394 <115027251+924394293492394@users.noreply.github.com>
2024-04-24 00:49:10 +0300

    Starting The Server
    https://start.spring.io/

---
+++ src/main/java/com/example/SIMPLE_REST_API/DemoRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.SIMPLE_REST_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestApplication.class, args);
+	}
+
+}
