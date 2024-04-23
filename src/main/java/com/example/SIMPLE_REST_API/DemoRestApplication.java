98ca0544e497e5b813d9f12c7a3d6dbccfa5b5dc
924394293492394 <115027251+924394293492394@users.noreply.github.com>
2024-04-24 01:14:08 +0300

    realize default getmapping response (main.class)
    @RestController
    @SpringBootApplication
    public class DemoRestApplication {
    
    	public static void main(String[] args) {
    		SpringApplication.run(DemoRestApplication.class, args);
    	}
    
    	@GetMapping
    	public String hello(){
    		return "Hello, World";
    	}
    }

--- src/main/java/com/example/SIMPLE_REST_API/DemoRestApplication.java
+++ src/main/java/com/example/SIMPLE_REST_API/DemoRestApplication.java
@@ -2,7 +2,10 @@ package com.example.SIMPLE_REST_API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoRestApplication {
 
@@ -10,4 +13,8 @@ public class DemoRestApplication {
 		SpringApplication.run(DemoRestApplication.class, args);
 	}
 
+	@GetMapping
+	public String hello(){
+		return "Hello, World";
+	}
 }
