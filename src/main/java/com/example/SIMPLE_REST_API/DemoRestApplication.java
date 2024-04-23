c718e76fdda22fbef7f71e809192ebc125a1f6fa
924394293492394 <115027251+924394293492394@users.noreply.github.com>
2024-04-24 01:46:02 +0300

    add controller.StudentController

--- src/main/java/com/example/SIMPLE_REST_API/DemoRestApplication.java
+++ src/main/java/com/example/SIMPLE_REST_API/DemoRestApplication.java
@@ -1,16 +1,8 @@
 package com.example.SIMPLE_REST_API;
 
-import com.example.SIMPLE_REST_API.entity.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-
-@RestController
 @SpringBootApplication
 public class DemoRestApplication {
 
@@ -18,10 +10,4 @@ public class DemoRestApplication {
 		SpringApplication.run(DemoRestApplication.class, args);
 	}
 
-	@GetMapping
-	public List<Student> hello(){
-		return List.of(
-				new Student(1L, "Egor", "koroleff2014@gmail.com", LocalDate.of(2004, Month.NOVEMBER, 24), 20)
-		);
-	}
 }
